f68619279d7c7d2ff02d79ca91d418ae19a1c9ca
tiago.ponte <tiago.ponte@redeinova.net>
2024-11-28 13:24:18 -0300

    🔧 chore: Correções para funcionamento do google maps e locator no IOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyD6N1Bi7N9NkV8VtpbI4L5aB6wDwxm82vA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
